9c4b1359fea28576394c4270858ea6080844874e
Bruce Cooner <brucecooner@gmail.com>
2017-01-28 01:46:11 -0700

    started moving code into mandala.js

---
+++ mandala.js
@@ -0,0 +1,77 @@
+function mandala_newConfig()
+{
+   return {
+      numPetals:5,        // how many 'slices of pie'
+      rotOffset:0.0,   // how much petals are rotate from 0 degrees (0..1)
+
+      // guide config
+      numPartialGuides:0, // how many sub-guides to draw between main spokes
+      guideOpacity:0.5,   // opacity of guide marks
+      // todo : stroke params for guides?
+   }
+}
+
+// -----------------------------------------------------------------------------
+function mandala_renderGuides(canvasElement, config)
+{
+   const radiansPerSpoke = TWO_PI / config.numPetals
+   let currentRotation = 0.0
+
+   boundRect = canvasElement.getBoundingClientRect();
+
+   const centerX = boundRect.width / 2.0
+   const centerY = boundRect.height / 2.0
+
+   const lineLength = boundRect.height / 2.0
+
+   var ctx = canvasElement.getContext("2d");
+   ctx.clearRect(0, 0, cvs.width, cvs.height);
+   ctx.setLineDash([0,0]);
+
+   canvasElement.style.opacity = config.guideOpacity
+
+   if (config.guideOpacity <= 0)
+   {
+      return
+   }
+
+   for (var currentSpoke = 0; currentSpoke < config.numPetals; ++currentSpoke)
+   {
+      rot_point = rotatePoint( 0.0, lineLength, currentRotation)
+      // rot_point.x *= centerX
+      // rot_point.y *= centerY
+
+      ctx.beginPath();
+      ctx.moveTo( centerX, centerY )
+      ctx.lineTo( centerX + rot_point.x,centerY + rot_point.y);
+      ctx.stroke();
+
+      currentRotation += radiansPerSpoke
+   }
+
+   let currentHalf = 0  // debug
+
+   if (config.numPartialGuides > 0)
+   {
+      let currentHalfRotation = radiansPerSpoke / (config.numPartialGuides + 1)
+
+      ctx.setLineDash([5, 3]);
+
+      for (var currentSpoke = 0; currentSpoke < config.numPetals; ++currentSpoke)
+      {
+         halfrot_point = rotatePoint( 0.0, lineLength, currentHalfRotation)
+         // halfrot_point.x *= centerX
+         // halfrot_point.y *= centerY
+         ctx.beginPath();
+         ctx.moveTo( centerX, centerY )
+         ctx.lineTo( centerX + halfrot_point.x,centerY + halfrot_point.y);
+         ctx.stroke();
+
+         currentHalfRotation += radiansPerSpoke
+
+         currentHalf += 1
+         console.log(`render half : ${currentHalf}`)
+      }
+   }
+
+}
